d4b8491b83303e795e2be86492f67993d35df1d7
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2024-02-17 18:56:11 +0500

    Добавить модели устройств

--- src/devices/models.py
+++ src/devices/models.py
@@ -1,3 +1,32 @@
+from datetime import date
+
 from django.db import models
 
-# Create your models here.
+
+class Device(models.Model):
+    parent_device = models.ForeignKey('self', on_delete=models.SET_NULL,
+                                      null=True)
+    name = models.CharField(max_length=200)
+    type = models.ForeignKey('DeviceType', on_delete=models.CASCADE)
+    created_at = models.DateField(default=date.today)
+    tech_place = models.ForeignKey('TechPlace', on_delete=models.SET_NULL,
+                                   null=True)
+
+    class Meta:
+        db_table = 'devices'
+
+
+class DeviceType(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'device_types'
+
+
+class TechPlace(models.Model):
+    name = models.CharField(max_length=200)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    class Meta:
+        db_table = 'tech_places'
